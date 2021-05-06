986a3e014217eba9eaf767be5604c32a312d2b6a
Katie Pizziketti <kpizziketti@gmail.com>
2021-05-05 20:25:56 -0400

    fixed bug with line input

--- CoverLetter.cpp
+++ CoverLetter.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main(){
     std::string input = "";
     cout << "Hello Potential Employer!  What's the name of the company do you want to recruit me into?\n";
-    cin >> input;
+    std::getline(std::cin, input);
+    while(input.std::string::empty()){
+        cout << "Please type the company name or use ctl+c to quit.\n";
+        std::getline(std::cin, input);
+    }
     writer::Author a;
     a.write(input);
 }
