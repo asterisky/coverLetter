e0c1c50b6c9ea968eec788d7541554075621ea1b
Katie Pizziketti <kpizziketti@gmail.com>
2021-05-07 11:13:00 -0400

    practicing with objects

--- CoverLetter.cpp
+++ CoverLetter.cpp
@@ -5,13 +5,36 @@ using namespace std;
 
 int main(){
     std::string input = "";
+    std::string companyName;
+    std::string positionName;
+    std::string postingSource;
     cout << "Hello Potential Employer!  What's the name of the company do you want to recruit me into?\n";
+
+    // get company name
     std::getline(std::cin, input);
     while(input.std::string::empty()){
         cout << "Please type the company name or use ctl+c to quit.\n";
         std::getline(std::cin, input);
     }
-    writer::Author a;
-    a.write(input);
-    cout << "I've always wanted to work at " << in << "!\n";
+    cout << "I've always wanted to work at " << in << "!\n";    
+    // get position
+    cout << "What position are you looking to fill at " << input << "?\n";
+    std::getline(std::cin, input);
+    while(input.std::string::empty()){
+        cout << "Please type the job title or use ctl+c to quit.\n";
+        std::getline(std::cin, input);
+    }
+
+    // get the source
+    cout << "What website did you post the listing to?\n";
+    std::getline(std::cin, input);
+    while(input.std::string::empty()){
+        cout << "Please type the source or use ctl+c to quit.\n";
+        std::getline(std::cin, input);
+    }
+
+    writer::Author a = new Author(companyName, positionName, postingSource);
+    
+    a.write(a);
+    
 }
